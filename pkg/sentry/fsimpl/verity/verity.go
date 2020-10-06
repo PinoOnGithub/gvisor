29d2d66051678618b4e28c665e838ee497629ce5
Dean Deng <deandeng@google.com>
2020-10-05 23:34:53 -0700

    Fix SCM Rights reference leaks.
    Control messages should be released on Read (which ignores the control message)
    or zero-byte Send. Otherwise, open fds sent through the control messages will
    be leaked.
    
    PiperOrigin-RevId: 335579592

--- pkg/sentry/fsimpl/verity/verity.go
+++ pkg/sentry/fsimpl/verity/verity.go
@@ -150,6 +150,9 @@ func isEnabled(d *dentry) bool {
 	return !d.fs.allowRuntimeEnable || len(d.rootHash) != 0
 }
 
+// Release implements vfs.FilesystemType.Release.
+func (FilesystemType) Release(ctx context.Context) {}
+
 // alertIntegrityViolation alerts a violation of integrity, which usually means
 // unexpected modification to the file system is detected. In
 // noCrashOnVerificationFailure mode, it returns an error, otherwise it panic.
